5f7f3e3105115b1d70f853501f6e534c290bdeb8
Sami <votre_email@votre_email.com>
2021-10-11 11:22:45 +0200

    Commit

--- PayMyBuddy/src/main/java/com/example/PayMyBuddy/controller/UserController.java
+++ PayMyBuddy/src/main/java/com/example/PayMyBuddy/controller/UserController.java
@@ -56,6 +56,26 @@ public class UserController {
         return "login";
     }
 
+    /*@GetMapping("/profile")
+    public String profile(Model model){
+        org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = springUser.getUsername();
+        Optional<User> user = userRepository.findByEmail(username);
+        model.addAttribute("user", user);
+        int id = userService.findUserId(username);
+        Account account = accountRepository.findByUserId(id);
+        model.addAttribute("account", account);
+        return "profile";*/
+
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+
+
     @GetMapping("/profile")
     public String profile(Model model){
         org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder
@@ -69,11 +89,5 @@ public class UserController {
         return "profile";
 
     }
-    @GetMapping("/contact")
-    public String contact(){
-        return "contact";
-    }
-
 
 }
-
