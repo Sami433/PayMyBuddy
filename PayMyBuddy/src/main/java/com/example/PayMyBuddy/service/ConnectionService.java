d6b37a0833cf9b7b0b57a14400e68c315442fa2c
Sami <votre_email@votre_email.com>
2021-10-18 10:27:23 +0200

    commit

--- PayMyBuddy/src/main/java/com/example/PayMyBuddy/service/ConnectionService.java
+++ PayMyBuddy/src/main/java/com/example/PayMyBuddy/service/ConnectionService.java
@@ -33,9 +33,9 @@ public class ConnectionService {
 
         org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder
                 .getContext().getAuthentication().getPrincipal();
-        String userx = springUser.getUsername();             // appeller le user x
-        int id = userService.findUserId(userx);     // grace a la methode qui permet de trouver un user par son mail:  trouver l'id de l'user x
-        User user1 = userRepository.findById(id);   // trouver l'id de l'user 1
+        String username = springUser.getUsername();
+        int id = userService.findUserId(username);
+        User user1 = userRepository.findById(id);
 
         String email = connection.getUser2().getEmail();
         Optional<User> user2 = userRepository.findByEmail(email);
@@ -48,49 +48,6 @@ public class ConnectionService {
 }
 
 
-
-
-
-
-
-
-
-   /* public void add(Connection connection) {
-        org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder
-                .getContext().getAuthentication().getPrincipal();
-        String username = springUser.getUsername();
-        int id = userService.findUserId(username);
-        User user1 = userRepository.findById(id);
-        //Add Connection if user is already in DB and isn't duplicate
-        List<Connection> listConnections = connectionRepository.findByUser1Id(id);
-        boolean isAlreadyInConnection = false;
-        String email = connection.getUser2().getEmail();
-        Optional<User> user2 = userRepository.findByEmail(email);
-
-        if (!user2.isEmpty()) {
-            for (Connection conn : listConnections) {
-                if (conn.getUser2().getEmail().equals(user2.get().getEmail())) {
-                    isAlreadyInConnection = true;
-                }
-            }
-
-            if (!isAlreadyInConnection) {
-                List<User> userList = userRepository.findAll();
-                for (User user : userList) {
-                    if (user.getEmail().equals(user2.get().getEmail())) {
-                        connection.setUser1(user1);
-                        connection.setUser2(user2.get());
-                        connectionRepository.save(connection);
-                    }
-                }
-            }
-        }
-
-    }
-
-
-}
-            */
 
 
 
