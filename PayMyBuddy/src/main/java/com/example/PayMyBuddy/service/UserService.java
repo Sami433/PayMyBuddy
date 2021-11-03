4dda9fa2820fe004554532e78973356b2f4e7983
Sami <votre_email@votre_email.com>
2021-11-03 12:46:34 +0100

    Commit

--- PayMyBuddy/src/main/java/com/example/PayMyBuddy/service/UserService.java
+++ PayMyBuddy/src/main/java/com/example/PayMyBuddy/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,6 +30,5 @@ public class UserService {
         Optional<User> user = userRepository.findByEmail(username);
         return user.get().getId();
     }
-
 
 }
